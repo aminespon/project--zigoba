d570854353860fc463f54617a01f5d1f00865118
fininfo\hsoussi <soussih@gmail.com>
2022-02-09 15:26:13 +0100

    add static server

--- project zigo/api/index.js
+++ project zigo/api/index.js
@@ -21,6 +21,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
